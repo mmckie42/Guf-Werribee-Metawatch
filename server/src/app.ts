e7a4a280ffdd2cde1ba4f973763cec1ae6b22a27
Michael <michael.mckie@gpsupport.com.au>
2023-08-30 14:01:01 +1000

    data now importing.

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,21 +13,26 @@ const env = app.get('env').trim();
 
 // Import data from csv section
 // using info from https://sebhastian.com/read-csv-javascript/
-fs.createReadStream('./csv-import/armies.csv')
-	.pipe(parse({ delimiter: ",", from_line: 14 }))
+// this will probably need to be its own section.
+const winData = []
+fs.createReadStream('./src/csv-import/armies.csv')
+	.pipe(parse({ delimiter: ",", columns: true, ltrim: true }))
 	.on("data", function (row) {
-		console.log(row)
+		// console.log(row)
+		winData.push(row)
 	})
 	.on("error", function (error) {
 		console.log(error.message);
 	})
 	.on("end", function () {
+		console.log(winData);
 		console.log("finished");
 	});
 
 
 // End import data section
-
+// the data is now imported and stored as winData.
+// Next step is to compile each faction into its own faction object with the combined data from winData
 
 app.get('/hello', async (req, res: express.Response<string>) => {
 	res.json('Hello world');
